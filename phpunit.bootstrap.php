ddd97fdc91274d824954f5ac16413872cea3767e
Grigorii Duca <contact@greg.md>
2017-05-26 21:32:08 +0300

    .

--- phpunit.bootstrap.php
+++ phpunit.bootstrap.php
@@ -1,16 +1,5 @@
 <?php
 
 ini_set('error_reporting', -1);
-
 ini_set('display_errors', 1);
-
 ini_set('display_startup_errors', 1);
-
-function dd($arg, ...$args)
-{
-    foreach (func_get_args() as $argument) {
-        print_r($argument);
-    }
-
-    die;
-}
